6f69c3e4ef4bb74fe01f396ae1cdcac7635278ab
faiq farooq <faiqfarooq777@gmail.com>
2023-03-10 22:41:45 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home';
+import Nopage from './Pages/Nopage';
 
 function App() {
   return (
-    <div >
-     lets start a app
-    </div>
+     <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/*' element={<Nopage/>}/>
+     </Routes>
+     </BrowserRouter>
   );
 }
 
